b1bfc5de1c99fa8a386f790fc16b01d0873a4499
Gerasimos Vasileiou <80413334+gvasss@users.noreply.github.com>
2025-01-29 21:10:12 +0200

    Update Footer.jsx

--- frontend-likeat/src/layout/Footer.jsx
+++ frontend-likeat/src/layout/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>© 2024 Copyright: LikEat</p>
+        <p>© 2024 Copyright: LikEat  |  support-likeat@protonmail.com</p></p>
       </div>
     </footer>
   );
